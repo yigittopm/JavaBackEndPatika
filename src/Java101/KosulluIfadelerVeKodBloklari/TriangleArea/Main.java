bc6f0c83a9d552f2b36045590a58b6f08bb96c8f
yigittopm <yigittopm@hotmail.com>
2021-07-22 13:57:11 +0300

    triangle area

---
+++ src/Java101/KosulluIfadelerVeKodBloklari/TriangleArea/Main.java
@@ -0,0 +1,26 @@
+package Java101.KosulluIfadelerVeKodBloklari.TriangleArea;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double side1, side2, side3, u, area;
+
+        System.out.print("Please enter side1 length: ");
+        side1 = scan.nextDouble();
+
+        System.out.print("Please enter side2 length: ");
+        side2 = scan.nextDouble();
+
+        System.out.print("Please enter side3 length: ");
+        side3 = scan.nextDouble();
+
+        u = (side1 + side2 + side3) / 2;
+        area = u * (u-side1) * (u-side2) * (u-side3);
+        area = Math.sqrt(area);
+
+        System.out.println("Triangle area: " + area);
+    }
+}
