fe2fef01b101f947719216727d7812202838779f
yigittopm <yigittopm@hotmail.com>
2021-07-22 12:11:11 +0300

    grade average completed

---
+++ src/Java101/KosulluIfadelerVeKodBloklari/GradeAverage/Main.java
@@ -0,0 +1,36 @@
+package Java101.KosulluIfadelerVeKodBloklari.GradeAverage;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your math grad: ");
+        double math = scan.nextDouble();
+
+        System.out.print("Please enter your physics grad: ");
+        double physics = scan.nextDouble();
+
+        System.out.print("Please enter your chemistry grad: ");
+        double chemistry = scan.nextDouble();
+
+        System.out.print("Please enter your turkish grad: ");
+        double turkish = scan.nextDouble();
+
+        System.out.print("Please enter your history grad: ");
+        double history = scan.nextDouble();
+
+        System.out.print("Please enter your music grad: ");
+        double music = scan.nextDouble();
+
+        double sum = math + physics + chemistry + turkish + history + music;
+        double average = sum / 6;
+
+        if(average >= 60) {
+            System.out.println("\n\nYou passed the class..("+average+")");
+        } else {
+            System.out.println("\n\nYou failed the class..("+average+")");
+        }
+    }
+}
--- src/Java101/Main.java
+++
@@ -1,8 +0,0 @@
-package Java101;
-
-public class Main {
-    public static void main(String[] args) {
-        Integer a = new Integer(10);
-        System.out.println("Girdiginiz sayı: " + a);
-    }
-}
