d553dd5414425fb0054c095fd5f117f7c898c353
yigittopm <yigittopm@hotmail.com>
2021-07-22 12:41:18 +0300

    kdv added

---
+++ src/Java101/KosulluIfadelerVeKodBloklari/KDV/Main.java
@@ -0,0 +1,26 @@
+package Java101.KosulluIfadelerVeKodBloklari.KDV;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double kdv = 0;
+        double priceWithKdv = 0;
+
+        System.out.print("Please enter quantity: ");
+        double quantity = scan.nextDouble();
+
+        if(quantity > 0 && quantity <= 1000) {
+            kdv = (quantity * 18) / 100;
+        } else {
+            kdv = (quantity * 8) / 100;
+        }
+        priceWithKdv = quantity + kdv;
+
+        System.out.println("Quantity: "+ quantity);
+        System.out.println("KDV: "+ kdv);
+        System.out.println("PriceWithKdv: "+ priceWithKdv);
+    }
+}
