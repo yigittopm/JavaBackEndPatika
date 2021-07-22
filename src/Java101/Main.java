6e346e619fe776f520ac0f4604d44b7bbf80eede
yigittopm <yigittopm@hotmail.com>
2021-07-22 10:45:42 +0300

    initial

---
+++ src/Java101/Main.java
@@ -0,0 +1,8 @@
+package Java101;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a = new Integer(10);
+        System.out.println("Girdiginiz sayı: " + a);
+    }
+}
