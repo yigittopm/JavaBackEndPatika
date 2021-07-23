c487434da2296619d6d9724ed74ddcad3518da59
yigittopm <yigittopm@hotmail.com>
2021-07-23 13:52:33 +0300

    greengrocer added

---
+++ src/Java101/KosulluIfadelerVeKodBloklari/Greengrocer/Main.java
@@ -0,0 +1,32 @@
+package Java101.KosulluIfadelerVeKodBloklari.Greengrocer;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double  total=0,
+                pear = 2.14,
+                apple = 3.67,
+                tomato = 1.11,
+                banana = 0.95,
+                aubergine = 5.00;
+
+        System.out.print("How many kilos of pear: ");
+        total += pear * scan.nextDouble();
+
+        System.out.print("How many kilos of apple: ");
+        total += apple * scan.nextDouble();
+
+        System.out.print("How many kilos of tomato: ");
+        total += tomato * scan.nextDouble();
+
+        System.out.print("How many kilos of banana: ");
+        total += banana * scan.nextDouble();
+
+        System.out.print("How many kilos of aubergine: ");
+        total += aubergine * scan.nextDouble();
+
+        System.out.println("Total: " + total);
+    }
+}
