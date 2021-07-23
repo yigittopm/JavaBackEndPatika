1f44a892a310430dfa8d24327e53c118fb0f9b1d
yigittopm <yigittopm@hotmail.com>
2021-07-23 13:44:42 +0300

    body mass index added

---
+++ src/Java101/KosulluIfadelerVeKodBloklari/BodyMassIndex/Main.java
@@ -0,0 +1,20 @@
+package Java101.KosulluIfadelerVeKodBloklari.BodyMassIndex;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.FRANCE);
+        double height, weight, index;
+
+        System.out.print("Please enter your height: ");
+        height = scan.nextDouble();
+
+        System.out.print("Please enter your weight: ");
+        weight = scan.nextDouble();
+
+        index = weight / (height * height);
+        System.out.println("Body mass index: " + index);
+    }
+}
